664470eafcbba88f8030d029ac1d3447564a769e
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2024-07-08 21:05:39 +0100

    student types

---
+++ src/api/modules/student/types/StudentTypes.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+import { createStudentSchema } from "../validators/StudentCreateSchema";
+
+export type CreateStudentData = z.infer<typeof createStudentSchema>;
+
+export interface StudentCriteria {
+  id?: number;
+  dob?: string;
+  address?: string;
+  enrollmentDate?: string;
+  classId?: number;
+  tenantId?: number;
+}
+
+export interface UpdateStudentData {
+  dob?: string;
+  address?: string;
+  enrollmentDate?: string;
+  classId?: number;
+  tenantId?: number;
+}
+
+export interface GetAndUpdateStaff {
+    criteria: StudentCriteria;
+    data: UpdateStudentData;
+    updateStatus?: boolean;
+}
