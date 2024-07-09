5344840b06880b70535bdf3ef23e042a474e6ec2
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2024-07-09 11:44:19 +0100

    role base validators

---
+++ src/api/modules/role/validators/RoleCreateSchema.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod';
+
+export const createRoleSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  tenantId: z.number().int("Tenant ID must be an integer"),
+});
+
