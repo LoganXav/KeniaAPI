fbc2cf0ad4105172180af690715c427070ce0050
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2024-07-09 11:48:39 +0100

    group base types

---
+++ src/api/modules/group/types/GroupTypes.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+import { createRoleSchema } from "../validators/RoleCreateSchema";
+
+export type CreateRoleData = z.infer<typeof createRoleSchema>;
+
+export interface RoleCriteria {
+  id?: number;
+  name?: string;
+  tenantId?: number;
+}
+
+export interface UpdateRoleData {
+    name?: string;
+    tenantId?: number;
+  }
+
+export interface GetAndUpdateRole {
+    criteria: RoleCriteria;
+    data: UpdateRoleData;
+    updateStatus?: boolean;
+}
