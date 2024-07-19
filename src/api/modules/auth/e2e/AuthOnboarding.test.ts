5a6b72df386960bf0ed46a894dc490bb0ea9e392
Segun <sogbesansegun22@gmail.com>
2024-07-19 19:27:14 +0100

    Fix: update auth e2e tests

--- src/api/modules/auth/e2e/AuthOnboarding.test.ts
+++ src/api/modules/auth/e2e/AuthOnboarding.test.ts
@@ -1,15 +1,17 @@
 import { Server } from "http";
 import request from "supertest";
 import { container } from "tsyringe";
+import { PrismaClient } from "@prisma/client";
 import TokenProvider from "../providers/Token.provider";
 import AuthSignInService from "../services/AuthSignIn.service";
 import AuthSignUpService from "../services/AuthSignUp.service";
 import StaffReadProvider from "../../staff/providers/StaffRead.provider";
 import { Application } from "../../../../infrastructure/internal/application";
 import AuthOnboardingController from "../controllers/AuthOnboarding.controller";
+import { HttpStatusCodeEnum } from "../../../shared/helpers/enums/HttpStatusCode.enum";
 import UserInternalApiProvider from "../../../shared/providers/user/UserInternalApi.provider";
 import TenantInternalApiProvider from "../../../shared/providers/tenant/TenantInternalApi.provider";
-import { PrismaClient } from "@prisma/client";
+import { ACCOUNT_CREATED, SIGN_IN_SUCCESSFUL, SUCCESS } from "../../../shared/helpers/messages/SystemMessages";
 
 describe("Auth Onboarding", () => {
   let server: Server;
@@ -62,22 +64,22 @@ describe("Auth Onboarding", () => {
       firstName: "Luke",
       lastName: "Combs",
       phoneNumber: "09052916792",
-      email: "sogbesansegun1@gmail.com",
+      email: "sogbesansegun3@gmail.com",
       password: "123456",
     });
 
-    expect(response.status).toBe(201);
+    expect(response.status).toBe(HttpStatusCodeEnum.CREATED);
     expect(response.body).toMatchObject({
-      status: "success",
-      statusCode: 201,
+      status: SUCCESS,
+      statusCode: HttpStatusCodeEnum.CREATED,
       data: {
-        message: "Account Created Successfully",
+        message: ACCOUNT_CREATED,
         data: {
           id: expect.any(Number),
           firstName: "Luke",
           lastName: "Combs",
           phoneNumber: "09052916792",
-          email: "sogbesansegun1@gmail.com",
+          email: "sogbesansegun3@gmail.com",
           hasVerified: expect.any(Boolean),
           isFirstTimeLogin: expect.any(Boolean),
           lastLoginDate: expect.any(String),
@@ -90,24 +92,24 @@ describe("Auth Onboarding", () => {
 
   it("Should sign in a user successfully", async () => {
     const response = await request(server).post("/api/auth/signin").send({
-      email: "sogbesansegun1@gmail.com",
+      email: "sogbesansegun3@gmail.com",
       userType: "Student",
       password: "123456",
     });
 
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(HttpStatusCodeEnum.SUCCESS);
     expect(response.body).toMatchObject({
-      status: "success",
-      statusCode: 200,
+      status: SUCCESS,
+      statusCode: HttpStatusCodeEnum.SUCCESS,
       data: {
-        message: "Sign In Successful",
+        message: SIGN_IN_SUCCESSFUL,
         data: {
           user: {
             id: expect.any(Number),
             firstName: "Luke",
             lastName: "Combs",
             phoneNumber: "09052916792",
-            email: "sogbesansegun1@gmail.com",
+            email: "sogbesansegun3@gmail.com",
             hasVerified: expect.any(Boolean),
             isFirstTimeLogin: expect.any(Boolean),
             lastLoginDate: expect.any(String),
--- src/api/modules/auth/e2e/AuthPasswordReset.test.ts
+++ src/api/modules/auth/e2e/AuthPasswordReset.test.ts
@@ -5,8 +5,11 @@ import TokenProvider from "../providers/Token.provider";
 import AuthPasswordResetService from "../services/AuthPasswordReset.service";
 import { Application } from "../../../../infrastructure/internal/application";
 import AuthPasswordResetController from "../controllers/AuthPasswordReset.controller";
+import { HttpStatusCodeEnum } from "../../../shared/helpers/enums/HttpStatusCode.enum";
 import AuthPasswordResetRequestService from "../services/AuthPasswordResetRequest.service";
 import UserInternalApiProvider from "../../../shared/providers/user/UserInternalApi.provider";
+import { ERROR, ERROR_INVALID_TOKEN, PASSWORD_RESET_LINK_GENERATED, SUCCESS } from "../../../shared/helpers/messages/SystemMessages";
+import { PrismaClient } from "@prisma/client";
 
 describe("Auth Password Reset", () => {
   let server: Server;
@@ -35,30 +38,57 @@ describe("Auth Password Reset", () => {
   authPasswordResetController = container.resolve(AuthPasswordResetController);
 
   afterAll(async () => {
-    server.close();
+    const prisma = new PrismaClient();
+    try {
+      await prisma.$transaction([prisma.userToken.deleteMany()]);
+
+      await prisma.user.deleteMany();
+      await prisma.tenant.deleteMany();
+
+      await prisma.user.deleteMany();
+    } catch (error) {
+      console.error("Error during cleanup:", error);
+    } finally {
+      await prisma.$disconnect();
+      server.close();
+    }
   });
 
   it("Should handle user password reset request successfully", async () => {
+    await request(server).post("/api/auth/signup").send({
+      firstName: "Luke",
+      lastName: "Combs",
+      phoneNumber: "09052916792",
+      email: "sogbesansegun1@gmail.com",
+      password: "123456",
+    });
+
     const response = await request(server).get("/api/auth/password-reset/request").send({
       email: "sogbesansegun1@gmail.com",
     });
 
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(HttpStatusCodeEnum.SUCCESS);
     expect(response.body).toMatchObject({
-      status: "success",
-      statusCode: 200,
+      status: SUCCESS,
+      statusCode: HttpStatusCodeEnum.SUCCESS,
       data: {
-        message: "Password Recovery Link Generated. Please Check your mail",
+        message: PASSWORD_RESET_LINK_GENERATED,
       },
     });
-  });
+  }, 10000);
 
-  it("Should handle user password request successfully", async () => {
-    const response = await request(server).post("/api/auth/password-reset/:12345").send({
+  it("Should handle invalid user password reset", async () => {
+    const response = await request(server).post("/api/auth/password-reset/12345").send({
       password: "password",
     });
 
-    expect(response.status).toBe(200);
-    // expect(response.body).toEqual(mockResult.toResultDto());
+    expect(response.status).toBe(400);
+    expect(response.body).toMatchObject({
+      status: ERROR,
+      statusCode: HttpStatusCodeEnum.BAD_REQUEST,
+      data: {
+        message: ERROR_INVALID_TOKEN,
+      },
+    });
   });
 });
