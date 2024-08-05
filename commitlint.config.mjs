bc67a8cb6c1994928b428d292d95657c4e0ed253
Segun <sogbesansegun22@gmail.com>
2024-08-05 17:31:46 +0100

    Chore: update configurations

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,6 +2,7 @@ export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "type-enum": [2, "always", ["Feat", "Fix", "Chore", "Docs", "Style", "Refactor", "Test"]],
+    "type-case": [0, "never", ["lower-case"]],
     "subject-case": [0, "never", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
   },
 };
