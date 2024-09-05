99d0239081ab221fbc7aa32b3c0e6ce1f8eacd25
Segun <sogbesansegun22@gmail.com>
2024-09-05 16:21:39 +0100

    Fix: deployment fixes

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -5,12 +5,18 @@ set -e
 
 cd /home/ec2-user/apps/kenia/backend || exit 1
 
-git pull origin master
+# Discard any untracked files and directories
+git clean -fd
+
+# Reset local changes and forcefully pull the latest code from master
+git fetch --all
+git reset --hard origin/master
 
 docker-compose down
+
 docker-compose up --build -d
 
-docker exec -it backend-kenia-api-1 pnpm run prisma:stage-generate
+# docker exec -it backend-kenia-api-1 pnpm run prisma:stage-generate
 docker exec -it backend-kenia-api-1 pnpm run prisma:stage-migrate
 
 echo "Deployment completed successfully!"
