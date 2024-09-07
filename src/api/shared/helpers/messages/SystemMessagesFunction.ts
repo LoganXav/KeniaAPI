9162efd0b7c2068c5fe27644f06df0ffb11df4cc
Segun <sogbesansegun22@gmail.com>
2024-09-07 12:43:25 +0100

    Chore: modify system messages and password reset method

--- src/api/shared/helpers/messages/SystemMessagesFunction.ts
+++ src/api/shared/helpers/messages/SystemMessagesFunction.ts
@@ -1,15 +1,15 @@
 export function RESOURCE_FETCHED_SUCCESSFULLY(resourceName = "Resource") {
-  return ` ${resourceName} Fetched Successfully`;
+  return ` ${resourceName} fetched successfully`;
 }
 
 export function RESOURCE_RECORD_NOT_FOUND(resourceName = "Resource") {
-  return `${resourceName} Record Was not found`;
+  return `${resourceName} record was not found`;
 }
 
 export function RESOURCE_RECORD_CREATED_SUCCESSFULLY(resourceName = "Resource") {
-  return `${resourceName} Record Created Successfully`;
+  return `${resourceName} record created successfully`;
 }
 
 export function RESOURCE_RECORD_UPDATED_SUCCESSFULLY(resourceName = "Resource") {
-  return `${resourceName} Record Updated Successfully`;
+  return `${resourceName} record updated successfully`;
 }
