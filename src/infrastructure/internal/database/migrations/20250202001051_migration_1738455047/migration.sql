6255ac719d32c37c967ee00d807e3398f22f42a6
Segun <sogbesansegun22@gmail.com>
2025-02-02 01:30:27 +0100

    Chore: remove migrations

--- src/infrastructure/internal/database/migrations/20250202001051_migration_1738455047/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `nin` on the `Staff` table. The data in that column could be lost. The data in that column will be cast from `BigInt` to `Char(11)`.
-
-*/
--- AlterTable
-ALTER TABLE "Staff" ALTER COLUMN "nin" SET DATA TYPE CHAR(11);
--- src/infrastructure/internal/database/migrations/20250202001642_migration_1738455400/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Staff" ALTER COLUMN "nin" SET DATA TYPE TEXT;
