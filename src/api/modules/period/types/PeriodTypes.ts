91b7f38b4b96885c67afcc5072011458b70db8da
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2025-03-17 13:11:39 +0100

    Chore: period module

---
+++ src/api/modules/period/types/PeriodTypes.ts
@@ -0,0 +1,62 @@
+import { Subject } from "@prisma/client";
+
+export type PeriodCreateRequestType = {
+  id?: number;
+  startTime: string;
+  endTime: string;
+  subjectId?: number;
+  timetableId: number;
+  isBreak: boolean;
+  breakType?: "SHORTBREAK" | "LONGBREAK";
+  tenantId: number;
+};
+
+export type PeriodReadRequestType = {
+  ids?: number[];
+  timetableId?: number;
+  subjectId?: number;
+  tenantId: number;
+};
+
+export type PeriodReadOneRequestType = {
+  id?: number;
+  timetableId?: number;
+  subjectId?: number;
+  tenantId?: number;
+};
+
+export type PeriodUpdateRequestType = {
+  id: number;
+  startTime?: string;
+  endTime?: string;
+  subjectId?: number;
+  timetableId?: number;
+  isBreak?: boolean;
+  breakType?: "SHORTBREAK" | "LONGBREAK";
+  tenantId?: number;
+};
+
+export type PeriodCriteriaType = {
+  id?: number;
+  ids?: number[];
+  timetableId?: number;
+  subjectId?: number;
+  tenantId?: number;
+};
+
+export type PeriodResponseType = {
+  id: number;
+  startTime: string;
+  endTime: string;
+  subjectId: number | null;
+  timetableId: number;
+  isBreak: boolean;
+  breakType: "SHORTBREAK" | "LONGBREAK" | null;
+  tenantId: number;
+  subject?: Subject;
+};
+
+export type PeriodDeleteRequestType = {
+  id: number;
+  tenantId: number;
+};
