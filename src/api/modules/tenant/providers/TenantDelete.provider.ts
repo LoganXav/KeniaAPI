8e13cbcf5ee4fb99922cbd27d817832a703c8676
Segun <sogbesansegun22@gmail.com>
2025-04-09 16:17:37 +0100

    Fix: deployment fix

--- src/api/modules/tenant/providers/TenantDelete.provider.ts
+++ src/api/modules/tenant/providers/TenantDelete.provider.ts
@@ -1,17 +1,17 @@
 import DbClient, { PrismaTransactionClient } from "~/infrastructure/internal/database";
 import { Tenant } from "@prisma/client";
-import { TenantCriteria } from "../types/TenantTypes";
+import { TenantReadCriteria } from "../types/TenantTypes";
 
 export default class StaffDeleteProvider {
-  public async deleteOneByCriteria(criteria: TenantCriteria, dbClient: PrismaTransactionClient = DbClient): Promise<Tenant | any> {
-    const { id } = criteria.id;
+  public async deleteOneByCriteria(criteria: TenantReadCriteria, dbClient: PrismaTransactionClient = DbClient): Promise<Tenant | any> {
+    const { id } = criteria;
     const deletedTenant = await dbClient?.tenant?.delete({
       where: { id },
     });
     return deletedTenant;
   }
 
-  public async deleteByCriteria(criteria: TenantCriteria, dbClient: PrismaTransactionClient = DbClient): Promise<Tenant | any> {
+  public async deleteByCriteria(criteria: TenantReadCriteria, dbClient: PrismaTransactionClient = DbClient): Promise<Tenant | any> {
     const deletedTenant = await dbClient?.tenant?.deleteMany({
       where: criteria,
     });
