692fad817bbbfcf1b17299605bb8d8527a264f2a
Segun <sogbesansegun22@gmail.com>
2025-04-12 22:25:13 +0100

    Chore: add routes that skip decryption

--- src/infrastructure/internal/middleware/decryption/index.ts
+++ src/infrastructure/internal/middleware/decryption/index.ts
@@ -2,12 +2,12 @@ import { Request, Response, NextFunction } from "express";
 import { Middleware } from "~/infrastructure/internal/types";
 import { PayloadEncryptService } from "~/api/shared/services/encryption/PayloadEncryptService";
 import ServerConfig, { DEV } from "~/config/ServerConfig";
-import { NOT_ENCRYPTED } from "~/config/RoutesConfig";
+import { SKIP_DECRYPTION } from "~/config/RoutesConfig";
 import { BooleanUtil } from "~/utils/BooleanUtil";
 
 class DecryptionMiddleware {
   public handle: Middleware = (req: Request, _res: Response, next: NextFunction): void => {
-    const skipDecryption = NOT_ENCRYPTED.some((path) => BooleanUtil.areEqual(path, req.path));
+    const skipDecryption = SKIP_DECRYPTION.some((path) => BooleanUtil.areEqual(path, req.path));
 
     if (ServerConfig.Environment !== DEV || !skipDecryption) {
       const decryptedRequestBody = PayloadEncryptService.decrypt(req.body.request);
