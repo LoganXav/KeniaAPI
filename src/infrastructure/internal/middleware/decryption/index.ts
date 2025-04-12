bc51e7fb54bd2c08edb5c8d46639c81c038b2324
Segun <sogbesansegun22@gmail.com>
2025-04-12 22:00:33 +0100

    Chore: change control flow for encrytion toggle

--- src/infrastructure/internal/middleware/decryption/index.ts
+++ src/infrastructure/internal/middleware/decryption/index.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import { Middleware } from "~/infrastructure/internal/types";
 import { PayloadEncryptService } from "~/api/shared/services/encryption/PayloadEncryptService";
+import ServerConfig, { DEV } from "~/config/ServerConfig";
 
 class DecryptionMiddleware {
   public handle: Middleware = (req: Request, _res: Response, next: NextFunction): void => {
-    if (process.env.NODE_ENV === "production") {
+    if (ServerConfig.Environment !== DEV) {
       const decryptedRequestBody = PayloadEncryptService.decrypt(req.body.request);
 
       req.body = decryptedRequestBody;
