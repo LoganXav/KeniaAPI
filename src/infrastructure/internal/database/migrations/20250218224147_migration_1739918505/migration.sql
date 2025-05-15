4b74343302272c0e753ac1217aa914eaf6d8252b
Segun <sogbesansegun22@gmail.com>
2025-05-15 13:47:21 +0100

    Fix: prisma migration

--- src/infrastructure/internal/database/migrations/20250218224147_migration_1739918505/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `name` on the `Class` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Class" DROP COLUMN "name";
--- src/infrastructure/internal/database/migrations/20250513214852_migration_1747172927/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name,tenantId]` on the table `Permission` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Permission_name_tenantId_key" ON "Permission"("name", "tenantId");
