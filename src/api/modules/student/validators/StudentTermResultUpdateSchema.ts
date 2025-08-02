6e73d1a74cf1e642a3bb8b11b1cc2697ab353494
Segun <sogbesansegun22@gmail.com>
2025-08-02 12:09:52 +0100

    Chore: change term result finalized flag and error message

--- src/api/modules/student/validators/StudentTermResultUpdateSchema.ts
+++ src/api/modules/student/validators/StudentTermResultUpdateSchema.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const StudentTermResultUpdateRequestSchema = z.object({
   tenantId: z.number({ required_error: "Tenant Id is required", invalid_type_error: "Tenant Id must be a numner" }),
   userId: z.number({ required_error: "User Id is required", invalid_type_error: "User Id must be a number" }),
-  finalized: z.boolean({ required_error: "finalized is required" }),
+  finalized: z.boolean({ required_error: "Finalized is required" }),
 });
